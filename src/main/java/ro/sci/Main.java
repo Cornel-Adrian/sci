040c91473b405fd366a1763a6d13277f57ca57b8
Cornel-Adrian <co96grig@gmail.com>
2018-09-06 22:43:06 +0300

    String methods done

--- src/main/java/ro/sci/Main.java
+++ src/main/java/ro/sci/Main.java
@@ -1,11 +1,21 @@
 package main.java.ro.sci;
 
+import main.java.ro.sci.StringsTest.StringUtility;
+
+import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public void main(String[] args) {
+        String input = scanner.nextLine();
+        System.out.println(input);
+
+        StringUtility stringUtility = new StringUtility(input);
+
 
     }
+
 
 }
